5ba73fdf7da6c246b624db48dfbabfd0b0009bb9
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: Preliminary support for Screw'd product builds
    If building a screwd_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the vendor/screwd/products/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for screwd makefiles.
    
    build: Fix fastpath code for SCREWD_BUILD
    
     * We need to adjust to the new validations
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -132,6 +132,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^screwd_") ; then
+       SCREWD_BUILD=$(echo -n $1 | sed -e 's/^screwd_//g')
+    else
+       SCREWD_BUILD=
+    fi
+    export SCREWD_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
