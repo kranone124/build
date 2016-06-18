d7e384df484ec6234b7fd1e3f8fdaf5b6f07af31
Alex Naidis <alex.naidis@linux.com>
2016-06-18 19:38:12 +0200

    core: combo: armv8-a: specific kryo tuning
    * cortex-a57 tuning is currently the best for kryo
    * The "kryo" tuning in clang, seems to be real bad
    
    Change-Id: I5c3bdeb931b98e786a7173b34a3dd416f14f7f96
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    # Conflicts:
    #	core/combo/arch/arm64/armv8-a.mk

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,5 +1,18 @@
+ifneq (,$(filter kryo,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+	arch_variant_cflags := -mcpu=cortex-a57
+else
 ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
 else
 	arch_variant_cflags :=
+endif
+endif
+
+ifneq (,$(filter cortex-a53 default,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+	arch_variant_cflags  += -mfix-cortex-a53-835769
+	arch_variant_ldflags := -Wl,--fix-cortex-a53-843419
+else
+	arch_variant_cflags  += -mno-fix-cortex-a53-835769
+	arch_variant_ldflags := -Wl,--no-fix-cortex-a53-843419
+	RS_DISABLE_A53_WORKAROUND := true
 endif
