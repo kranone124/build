6d5489a54b995c1810750761af96d1d2493d0abf
Corey Edwards <ensabahnur16@gmail.com>
2017-09-01 10:54:18 -0500

    Choppin em up...pt1
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -465,9 +465,23 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
     system_progress -= 0.1
+    
+  script.Print("******************************************")
+  script.Print("                                   /\ .___")
+  script.Print("  ______ ___________   ______  _  _)/_| _/")
+  script.Print(" /  ___// ___\_  __ \_/ __ \ \/ \/ / __ | ")
+  script.Print(" \___  \  \___|  | \/\  ___/\     / /_/ | ")
+  script.Print("/____  >\___  >__|    \___  >\/\_/\____ | ")
+  script.Print("     \/     \/            \/           \/ ")
+  script.Print("  ")
+  script.Print("******************************************")
+  device = GetBuildProp("ro.product.device", OPTIONS.info_dict)
+  model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+  modver = GetBuildProp("ro.screwd.version", OPTIONS.info_dict)
+  script.Print(" ")
+  script.Print("Device: %s (%s)"%(model, device))
+  script.Print("Version: %s"%(modver)); 
 
-  # Place a copy of file_contexts.bin into the OTA package which will be used
-  # by the recovery program.
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
@@ -499,6 +513,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
 
   script.ShowProgress(0.05, 5)
+  script.Print("Flashing kernel...")
   script.WriteRawImage("/boot", "boot.img")
 
   script.ShowProgress(0.2, 10)
@@ -508,6 +523,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.AppendExtra(OPTIONS.extra_script)
 
   script.UnmountAll()
+  script.Print("You are now Screw'd! Please reboot!")  
 
   if OPTIONS.wipe_user_data:
     script.ShowProgress(0.1, 10)
