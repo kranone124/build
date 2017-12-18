nasty007/android_build

Build: Fix breakfast
Breakfast is a special pass through to lunch, which was accidentally
removed in the merging of the AOSP and CAF branches. Add it back!

Change-Id: Ie185830605993d784b14c60fd673afcd9735acc7
Fixes: 2a373c908ef0 ("One branch to rule them all!")
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

envsetup.sh
@@ -581,6 +581,20 @@ function breakfast()
         unset aosp
     fi
 
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the du model name
+            lunch du_$target-userdebug
+        fi
+    fi
+
     return $?
 }
 

kranone124/build

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

envsetup.sh
@@ -591,6 +591,20 @@ function breakfast()
     unset aosp
     fi
 
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the du model name
+            lunch screwd_$target-userdebug
+        fi
+    fi
+
     return $?
 }
 